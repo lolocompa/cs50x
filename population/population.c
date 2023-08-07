6b0dda4672642b442997a2591611aa71b30e7458
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-07 23:44:02 +0000

    Mon, Aug 7, 2023, 8:44 PM -03:00

--- population/population.c
+++ population/population.c
@@ -13,10 +13,12 @@ int main(void)
     while (n < 9 || e < n);
 
 
-    int years = 0;
+    
 
     while(e < n)
     {
+
+        int years = 0;
         int x = e/3;
         int y = e/4;
         e = e + x - y;
