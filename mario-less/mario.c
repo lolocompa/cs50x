1a8b6f3a01224fc8d769c8383f67a807c03c696b
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-09 23:12:07 +0000

    Wed, Aug 9, 2023, 8:12 PM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -15,7 +15,8 @@ int main(void)
 
     for(int x = 0; x<height; x++)
     {
-        for(int y = 0; y<height; y++)
+        int n = height--;
+        for(int y = 0; y<n; y++)
         {
 
             printf("#");
