007527356652c5722d207166d5270277b4980390
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-15 22:44:36 +0000

    Tue, Aug 15, 2023, 7:44 PM -03:00

--- scrabble/test.c
+++ scrabble/test.c
@@ -5,11 +5,15 @@
 
 int main(void)
 {
-    string string = get_string("char: ");
+    string string = get_string("string: ");
     int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'};
-    letters[0] = POINTS[6];
-    printf("%i", string);
+    for(int i = 0; i < 26; i++)
+    {
+        letters[i] = POINTS[i];
+    }
+    printf("%i\n", string[0]);
+
 
 }
 
