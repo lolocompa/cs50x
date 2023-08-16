84f90ce750c1a9a751414c934fd2c8c4744f3db1
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-16 00:30:12 +0000

    Tue, Aug 15, 2023, 9:30 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -25,12 +25,12 @@ int main(void)
 
 int compute_score(string word)
 {
-    
+    int result = 0;
     int lenght = strlen(word);
     for(int i = 0; i < lenght; i++)
     {
         word[i] = POINTS[97 - word[i]];
-
+        result = word[i] + word [i - 1];
     }
     return result;
 }
