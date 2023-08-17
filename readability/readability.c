6689b3171ea7d8a328404daaf93a106efe7040e4
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-17 00:15:14 +0000

    Wed, Aug 16, 2023, 9:15 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -18,20 +18,16 @@ int main(void)
 int count_letters(string text)
 {
     int lenght = strlen(text);
-    int spaces = 0;
-    int punctuation = 0;
+    int puntuation = 0;
     for(int i = 0; i < lenght; i++)
     {
-        if (text[i] == 32)
-        {
-        spaces++;
-        }
+
         if ( text[i] < 97)
         {
-            punctuation++;
+            puntuation++;
         }
     }
-    int letters = lenght - spaces - punctuation;
+    int letters = lenght - puntuation;
     return letters;
 
 }
