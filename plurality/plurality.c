a1bd1270724d6b3f6f84dce6e864b784192ede2b
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-24 15:12:47 +0000

    Thu, Aug 24, 2023, 12:12 PM -03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,11 +83,11 @@ bool vote(string name)
         if (name == candidates[i].name)
         {
             candidates[i].votes++;
-            return false;
+            return 1;
         }
     }
 
-    return false;
+    return 0;
 
 
 }
