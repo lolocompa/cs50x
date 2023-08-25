d08ed5bd8053056b2acf3b9080a59a04a397fa69
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-25 23:16:39 +0000

    Fri, Aug 25, 2023, 8:16 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,10 +130,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for(int i = 0; i < voter_count; i++)
-    {
-        
-    }
+    
 }
 
 
