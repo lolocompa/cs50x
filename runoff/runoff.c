ecd15a08fbab997fc74242accc03bdc7f718bcad
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-26 15:13:18 +0000

    Sat, Aug 26, 2023, 12:13 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -157,7 +157,6 @@ void tabulate(void)
     for(int i = 0, int j = 0; i < voter_count; i++)
     {
         int index_vote = preferences[i][j];
-        for(int k = 0; )
         if (strcmp(candidates[index_vote].name, "eliminated") != 0)
         {
             candidates[index_vote].votes++;
