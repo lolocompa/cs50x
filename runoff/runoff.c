c841a3283327b11eaf22e98e2ebf22af9974baa2
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-26 15:39:23 +0000

    Sat, Aug 26, 2023, 12:39 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,7 +181,7 @@ bool print_winner(void)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > candidate_count/2)
+        if (candidates[i].votes > voter_count/2)
         {
             printf("%s\n", candidates[i].name);
             return 1;
