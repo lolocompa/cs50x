b111fb4cecc66ff7f47b21ccb813fe6ba3c506f9
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-08-27 22:41:44 +0000

    Sun, Aug 27, 2023, 10:41 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -121,7 +121,7 @@ int main(int argc, string argv[])
         // Eliminate anyone with minimum number of votes
         eliminate(min);
         printf("%s\n", candidates[2].name);
-        printf("%s\n", candidates[1].name);
+        printf("%i\n", candidates[2].votes);
 
 
         // Reset vote counts back to zero
