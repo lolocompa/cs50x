b995098ef378fbf0be980f3278958faaed11228b
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-02 16:59:45 +0000

    Sat, Sep 2, 2023, 1:59 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,10 +8,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for(int j = 0; j < width; j++)
         {
 
-            BYTE avereage = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3
-            image[i][j].rgbtRed = avereage
-            image[i][j].rgbtGreen = avereage
-            image[i][j].rgbtBlue = avereage
+            BYTE avereage = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3;
+            image[i][j].rgbtRed = avereage;
+            image[i][j].rgbtGreen = avereage;
+            image[i][j].rgbtBlue = avereage;
         }
     }
 }
