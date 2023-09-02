d2aa19d1fd1e901d2017842b91b80bb2e70746da
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-02 15:11:08 +0000

    Sat, Sep 2, 2023, 12:11 PM -03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            if (image[i][j].rgbtred
+            if (image[i][j].rgbtBlue == 0x00
         }
     }
 
