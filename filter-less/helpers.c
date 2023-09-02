a9425a88e4d16e0a7dc53de50cbd2e84b3d920ac
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-02 16:41:07 +0000

    Sat, Sep 2, 2023, 1:41 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,6 +7,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
+            int red_value = image[i][j].rgbtRed
+            int green_value = image[i][j].rgbtGreen
+            int blue_value = image[i][j].rgbtBlue
             
         }
     }
