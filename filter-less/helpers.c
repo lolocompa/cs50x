38cd25a04926727ec556cc918beb8ae05227d0bc
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-03 19:25:48 +0000

    Sun, Sep 3, 2023, 4:25 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -32,7 +32,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    int sepia[3] = 
+    int sepia[3] = [sepia]
 }
 
 
