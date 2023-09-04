3759be9feb136f9cc992d6530530218f7800e020
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-04 00:11:50 +0000

    Sun, Sep 3, 2023, 9:11 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -75,7 +75,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for(int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for(int j = 0; j < width/2; j++)
         {
             RGBTRIPLE *p1 = &image[i][j];
             RGBTRIPLE *p2 = &image[i][width - j];
