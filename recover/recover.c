1c7c4e318fff9400df2c38df4da4f6c493fe0bf4
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-06 23:56:40 +0000

    Wed, Sep 6, 2023, 8:56 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
     int block = 512;
     BYTE buffer[block];
     int file_count = 000;
-    BYTE j = 0xe;
-    BYTE i = j/2;
+    BYTE j = 0xe2;
+    uint8_t(j);
 
     while (fread(buffer, 1, block, forensic) == block)
     {
