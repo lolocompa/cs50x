6aef1771e4b7a9a88e12dbe948b71543b198031b
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-07 23:17:14 +0000

    Thu, Sep 7, 2023, 8:17 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     int block = 512;
     BYTE buffer[block];
     int file_count = 000;
-    string file_name[8];
+    char file_name[8];
 
     while (fread(buffer, 1, block, forensic) == block)
     {
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 
         if (first == 0xff && second == 0xd8 && third == 0xff && (fourth & 0xf0) == 0xe0)
         {
-            fclose()
+            fclose(file_name.jpeg);
 
         }
     }
