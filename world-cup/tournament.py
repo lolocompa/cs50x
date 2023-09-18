e40b499ef16e4b800ca00467bc718616f0846d7b
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-18 22:49:24 +0000

    Mon, Sep 18, 2023, 7:49 PM -03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -82,7 +82,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
 
-    while 
+    while len(teams) != 1:
+        
+
 
 
 
