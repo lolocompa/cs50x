dea15c3425db7e6b7fee60ce2e642e2bba71c7e1
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-18 22:55:53 +0000

    Mon, Sep 18, 2023, 7:55 PM -03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -43,7 +43,21 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    simulate_tournament(teams)
+
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        
+
+
+
+
+
+
+
+
+
+
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
