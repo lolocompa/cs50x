844d9da29e9200e5797f64def2542947f5a24f47
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-19 23:52:35 +0000

    Tue, Sep 19, 2023, 8:52 PM -03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,2 +1,31 @@
 def main():
-    int 
+    cents = get_cents()
+
+    quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
+
+    coins = quarters + dimes + cents + pennies
+
+    pribnt(f"{coins}")
+
+
+
+
+def calculate_quarters(cents):
+
+    qua = 0
+    while True:
+        n = cents - 25
+        if n < 0:
+            break
+        qua += 1
+    return qua
