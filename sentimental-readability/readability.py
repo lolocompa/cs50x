aa0030a148aa21eace6c0a0b09bc527947a06acb
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-20 23:00:31 +0000

    Wed, Sep 20, 2023, 8:00 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,4 +3,42 @@ import cs50
 
 def main():
     text = cs50.get_string("text: ")
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def count_letters(text):
+    length = len(text)
+    puntuation = 0
+    for let in range(length):
+        let = 
