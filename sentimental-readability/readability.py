4edf949b0b4da2c6462d19e325875f1b5d1d5106
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-09-20 23:15:10 +0000

    Wed, Sep 20, 2023, 8:15 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,34 +3,29 @@ import cs50
 
 def main():
     text = cs50.get_string("text: ")
+    letters = count_letters(text)
+    len_text = len(text)
 
+    sentences= 0
+    words = 1
+    for letter in range(len_text):
+        if letter == 32:
+            words += 1
+        elif letter == 46 or letter == 33 or letter == 63:
+            sentences +=1
 
+    wor = sentences / words
+    l = letters / words * 100
+    s = wor * 100
+    index = 0.0588 * l - 0.296 * s - 15.8
+    grade = index + 0.5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if grade > 16:
+        print("grade 16+")
+    elif grade < 1:
+        print("before grade 1")
+    else:
+        print(f"grade {grade}")
 
 
 
@@ -41,4 +36,10 @@ def count_letters(text):
     length = len(text)
     puntuation = 0
     for let in range(length):
-        let = 
+        let = tolower(let)
+        if let < 97:
+            puntuation += 1
+    let = length - puntuation
+    return let
+
+main()
