8ae757a1aa98d12123309020295821418857f3c9
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-08 22:41:42 +0000

    Sun, Oct 8, 2023, 7:41 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,16 @@
 
 -- description: Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
 -- Littering took place at 16:36. No known witnesses.
+
+
+
+-- to find the transcript of the witness
+ SELECT transcript
+   ...> FROM interviews
+   ...> WHERE month = '7'
+   ...> AND day = 28
+   ...> AND year = 2021
+   ...> ;
+
+
+--transcript:
