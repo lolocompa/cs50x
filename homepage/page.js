10b1135f92cf0f86dbce4e123acb7f4a23edd1bf
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-18 23:32:33 +0000

    Wed, Oct 18, 2023, 8:32 PM -03:00

--- homepage/page.js
+++ homepage/page.js
@@ -14,3 +14,41 @@ button1.addEventListener("click", function(event) {
         button1.textContent = "show pic 1"
     }
 });
+
+
+
+let button2 = document.querySelector("#button2");
+let pic2 = document.querySelector("#pic2");
+
+button2.addEventListener("click", function(event) {
+
+    if (pic2.style.display === "none")
+    {
+        pic2.style.display = "block";
+        button2.textContent = "hide pic 2";
+    }
+    else
+    {
+        pic2.style.display = "none";
+        button2.textContent = "show pic 2"
+    }
+});
+
+
+
+let button3 = document.querySelector("#button3");
+let pic3 = document.querySelector("#pic3");
+
+button3.addEventListener("click", function(event) {
+
+    if (pic3.style.display === "none")
+    {
+        pic3.style.display = "block";
+        button3.textContent = "hide pic 3";
+    }
+    else
+    {
+        pic3.style.display = "none";
+        button3.textContent = "show pic 3"
+    }
+});
