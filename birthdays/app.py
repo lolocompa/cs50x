f5855037e78c304e4cdd0323dcf376944cb4da58
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-22 22:56:26 +0000

    Sun, Oct 22, 2023, 7:56 PM -03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
-        db.execute()
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
 
 
         return redirect("/")
