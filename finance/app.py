503cb1e9b0a3c0df38a1619d7fa766b70e14b481
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-25 22:38:58 +0000

    Wed, Oct 25, 2023, 7:38 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -48,7 +48,8 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("buy.html")
 
 
 
