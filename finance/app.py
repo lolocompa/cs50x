e94df9c84a63b4464f2b821ca2f363a99d8b47ae
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-27 23:19:58 +0000

    Fri, Oct 27, 2023, 8:19 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -69,7 +69,7 @@ def index():
         price_total = market_price * float(shares)
         price_total_l.append(price_total)
 
-    times = len(name_l)
+    times = len(sym_l)
     return render_template("index.html", name_l=name_l, market_price_l=market_price_l, sym_l=sym_l, shares_l=shares_l, user_total=user_total[0]["cash"], price_total_l=price_total_l, times=times)
 
 
