59aee72aeb53653852331b5175a1707d3e772540
lolocompa <141460303+lolocompa@users.noreply.github.com>
2023-10-31 21:57:09 +0000

    Tue, Oct 31, 2023, 6:57 PM -03:00

--- my-project/app.py
+++ my-project/app.py
@@ -10,3 +10,24 @@ from helpers import apology, login_required
 
 
 app = Flask(__name__)
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+db = SQL("sqlite:///movies.db")
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
